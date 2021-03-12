00991147c43b68a265364eec3455b0f91eae217a
Anders Højer Jørgensen <virtualzeth@gmail.com>
2021-03-12 10:28:14 +0100

    removed additional iml changes

--- 04 Implementation/src/hoekcel/MainJarClass.java
+++ 04 Implementation/src/hoekcel/MainJarClass.java
@@ -4,7 +4,7 @@ import com.sun.tools.javac.Main;
 
 public class MainJarClass {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Main.main(args);
     }
 

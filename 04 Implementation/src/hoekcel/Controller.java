9a7e6f6dd2eae79470242c21740a7359db8e3a05
Anders Højer Jørgensen <virtualzeth@gmail.com>
2021-03-15 11:24:17 +0100

    qwe

--- 04 Implementation/src/hoekcel/Controller.java
+++
@@ -1,4 +0,0 @@
-package hoekcel;
-
-public class Controller {
-}
--- 04 Implementation/src/hoekcel/Main.java
+++ 04 Implementation/src/hoekcel/Main.java
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/mainView.fxml"));
+        root.getStylesheets().add(getClass().getResource("main.css").toExternalForm());
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 
